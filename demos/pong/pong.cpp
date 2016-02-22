6a1063ad68adf16bc47cf3e6489e766e65d8814c
Jesper Bergström <jes.bergstrom@gmail.com>
2016-02-22 20:01:02 +0200

    implemented app window

--- demos/pong/pong.cpp
+++ demos/pong/pong.cpp
@@ -1,4 +1,48 @@
+#include <iostream>
+#include <string>
+
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+
+void error(std::string message, std::string error) {
+    std::cout << message + error  << std::endl;
+}
+
 int main() {
+    // Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+	error("Could not initialize SDL: ", SDL_GetError());
+    }
+
+    SDL_Window* window = SDL_CreateWindow("leng++",
+					  100, 100,
+					  800, 600,
+					  SDL_WINDOW_OPENGL);
+    // Error checking
+    if(window == nullptr) {
+	error("Could not create window: ", SDL_GetError());
+    }
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    // Error checking
+    if(context == nullptr) {
+	error("Could not create GL context: ", SDL_GetError());
+    }
+
+    GLenum err = glewInit();
+
+    // Error checking
+    if(GLEW_OK != err) {
+	std::cout << "GLEW initialization error: " << glewGetErrorString(err) << std::endl;
+    }
+    std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << std::endl;
     
+    glEnable(GL_DEPTH_TEST);
+    
+    // Enable alpha blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     return 0;
 }
